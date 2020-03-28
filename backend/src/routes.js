d275de463ef78f057caa572529f9d4e8bcc6435a
Robert Santos <robertsantosti@outlook.com>
2020-03-28 20:52:52 -0300

    Validando informações da rota de crição de ongs

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const connection = require('./database/connection');
+const {celebrate, Segments, Joi} = require('celebrate');
 
 /** Controllers */
 const OngsController = require('./controllers/OngsController');
@@ -13,7 +13,15 @@ const routes = express.Router();
 
 /** Ongs */
 routes.get('/ongs', OngsController.index);
-routes.post('/ongs', OngsController.store);
+routes.post('/ongs', celebrate({
+    [Segments.BODY]: Joi.object().keys({
+        name: Joi.string().required(),
+        email: Joi.string().required().email(),
+        whatsapp: Joi.number().required().min(10).max(11),
+        city: Joi.string().required(),
+        uf: Joi.string().required().length(2),
+    })
+}),OngsController.store);
 
 /** Incidents */
 routes.get('/incidents', IncidentsController.index);
