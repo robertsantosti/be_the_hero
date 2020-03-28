275beba7f98a3a62bdcfb05542f688b223d3d70a
Robert Santos <robertsantosti@outlook.com>
2020-03-28 10:22:33 -0300

    Criando conexão com o banco de dados

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const crypto = require('crypto');
+const connection = require('./database/connection');
 
 const routes = express.Router();
 
-routes.post('/users', (req, res) => {
-    console.log(req.body);
-    return res.json({message: true});
+routes.post('/ongs', (req, res) => {
+
+    const {name, email, whatsapp, city, uf} = req.body;
+    const id = crypto.randomBytes(4).toString('HEX');
+
+    return res.json();
 });
 
 module.exports = routes;
