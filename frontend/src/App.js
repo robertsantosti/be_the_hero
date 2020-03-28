441ecfd2935c4dbfa6fa854fafa7f49f7f2af1dc
Robert Santos <robertsantosti@outlook.com>
2020-03-28 13:21:18 -0300

    Iniciando tela de login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
 
+import './global.css';
+
+/** Components */
+import Logon from "./pages/Logon";
+
 function App() {
   return (
-      <h1>Hello World</h1>
+    <Logon/>
   );
 }
 
