ca1d6fb0bd0fe301962d9fd640f31b16639404fb
Robert Santos <robertsantosti@outlook.com>
2020-03-28 09:44:34 -0300

    Instalando biblioteca nodemon (dev)

---
+++ backend/src/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+app.use(express.json());
+
+app.post('/users', (req, res) => {
+   console.log(req.body);
+   return res.json({message: true});
+});
+
+app.listen(3333);
