782d1f40d72eae9a407ebc60e83ede8b4bd5bc5b
Robert Santos <robertsantosti@outlook.com>
2020-03-28 13:25:49 -0300

    Instalando bibloteca de icones

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -2,13 +2,24 @@ import React from "react";
 
 import './style.css';
 import heroesImg from '../../assets/heroes.png';
+import logo from '../../assets/logo.svg';
 
 export default function Logon() {
     return (
         <div className="logon-container">
             <section className="form">
+                <img src={logo} alt="Be The Hero"/>
 
+                <form>
+                    <h1>Faça seu logon</h1>
+                    <input placeholder="Sua ID"/>
+                    <button type="submit">Entrar</button>
+
+                    <a href="/register">Não tenho cadastro</a>
+                </form>
             </section>
+
+            <img src={heroesImg} alt="Heroes"/>
         </div>
     );
 }
