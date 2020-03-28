c25fd11a29f70d3a3f99b8bf6ff02bdb1abb534e
Robert Santos <robertsantosti@outlook.com>
2020-03-28 11:59:29 -0300

    Instalando CORS

--- backend/src/controllers/IncidentsController.js
+++ backend/src/controllers/IncidentsController.js
@@ -20,6 +20,21 @@ module.exports = {
                 'ongs.uf',
             ]);
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         res.header('X-Total-Count', count['count(*)']);
 
         return res.json(incidents);
